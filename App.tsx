df55195217abfd08904150b9dd31c05cc9541367
Mateus Cesar <mateuscesarglima@gmail.com>
2023-05-13 13:31:07 -0300

    Fix splash screen time bug

--- App.tsx
+++ App.tsx
@@ -2,29 +2,31 @@ import { AuthProvider } from "@Context/auth";
 import { FichaProvider } from "@Context/sheets";
 import themes from "@Global/themes";
 import { Routes } from "@Routes/index";
-import { StatusBar } from "expo-status-bar";
-import React, { useCallback, useEffect, useState } from "react";
-import { Keyboard, TouchableWithoutFeedback } from "react-native";
 import {
   Lato_400Regular,
   Lato_700Bold,
   useFonts,
 } from "@expo-google-fonts/lato";
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useEffect, useState } from "react";
+import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
+
+import Entypo from "@expo/vector-icons/Entypo";
+import * as Font from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import Toast from "react-native-toast-message";
 import { ThemeProvider } from "styled-components";
-import Entypo from "@expo/vector-icons/Entypo";
-import * as Font from "expo-font";
 
-SplashScreen.preventAutoHideAsync();
-export default function App(): JSX.Element {
+SplashScreen.preventAutoHideAsync().catch((err) => console.log(err));
+
+export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
+  const [fontsLoaded] = useFonts({ Lato_400Regular, Lato_700Bold });
 
   useEffect(() => {
     const prepare = async () => {
       try {
         // Keep the splash screen visible while we fetch resources
-        await SplashScreen.preventAutoHideAsync();
         // Pre-load fonts, make any API calls you need to do here
         await Font.loadAsync(Entypo.font);
         // Artificially delay for two seconds to simulate a slow loading
@@ -40,6 +42,8 @@ export default function App(): JSX.Element {
     prepare();
   }, []);
 
+  console.log(appIsReady);
+
   const onLayoutRootView = useCallback(async () => {
     if (appIsReady) {
       // This tells the splash screen to hide immediately! If we call this after
@@ -52,25 +56,24 @@ export default function App(): JSX.Element {
   }, [appIsReady]);
 
   if (!appIsReady) {
-    return null as any;
+    return null;
   }
 
   return (
-    <TouchableWithoutFeedback
-      onPress={Keyboard.dismiss}
-      onLayout={onLayoutRootView}
-    >
-      {/* <NavigationContainer> */}
-      <ThemeProvider theme={themes}>
-        <AuthProvider>
-          <FichaProvider>
-            <Routes />
-            <StatusBar style="light" />
-            <Toast />
-          </FichaProvider>
-        </AuthProvider>
-      </ThemeProvider>
-      {/* </NavigationContainer> */}
-    </TouchableWithoutFeedback>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        {/* <NavigationContainer> */}
+        <ThemeProvider theme={themes}>
+          <AuthProvider>
+            <FichaProvider>
+              <Routes />
+              <StatusBar style="light" />
+              <Toast />
+            </FichaProvider>
+          </AuthProvider>
+        </ThemeProvider>
+        {/* </NavigationContainer> */}
+      </TouchableWithoutFeedback>
+    </View>
   );
 }
